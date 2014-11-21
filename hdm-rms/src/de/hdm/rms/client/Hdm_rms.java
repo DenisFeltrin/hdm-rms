572db7de34678b1c9c8e8c219d390e289e6c6b9f
alex <ap061@hdm-stuttgart.de>
2014-11-21 12:55:35 +0100

    RPC-Basisklassen erstellt (Service, AsyncService und Impl-Klasse).

--- hdm-rms/src/de/hdm/rms/client/Hdm_rms.java
+++ hdm-rms/src/de/hdm/rms/client/Hdm_rms.java
@@ -1,5 +1,23 @@
 package de.hdm.rms.client;
 
-class Hdm_rms {
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import de.hdm.rms.client.CreateUser;
+
+class Hdm_rms implements EntryPoint {
+	
+	
+	CreateUser Affe = new CreateUser();
+	
+	
+	public void onModuleLoad() {
+		
+		
+		RootPanel.get("content_wrap").add(Affe);
+		
+	}
+	
 	
 }
