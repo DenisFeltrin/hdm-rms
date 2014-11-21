416cef194d293919663c87a2164263c7c0663e1d
alex <ap061@hdm-stuttgart.de>
2014-11-21 14:09:01 +0100

    Create User Klasse Anpassung; HTML Anpassung

--- hdm-rms/src/de/hdm/rms/client/CreateUser.java
+++ hdm-rms/src/de/hdm/rms/client/CreateUser.java
@@ -4,38 +4,53 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class CreateUser extends VerticalPanel {
 
-	private final TextBox lastName = new TextBox();
 	private final VerticalPanel CreateUserPanel = new VerticalPanel();
-	private final Button userRegisterBtn = new Button("Registrieren");
-
+	private final Button userRegisterBtn = new Button("Nutzer anlegen");
+	private final TextBox firstName = new TextBox();
+	private final TextBox lastName = new TextBox();
+	private final TextBox nickName = new TextBox();
+	private final TextBox email = new TextBox();
+	private final Label firstNameLabel = new Label("Vorname");
+	private final Label lastNameLabel = new Label("Nachname");
+	private final Label nickNameLabel = new Label("Nickname");
+	private final Label emailLabel = new Label("Email");
+	
+	
+	
 	public CreateUser() {}
 	
 	
 	public void onLoad() {
 		
-		// set showcase Headline 
+		
+		RootPanel.get("content_wrap").add(CreateUserPanel);
+		CreateUserPanel.add(firstNameLabel);
+		CreateUserPanel.add(firstName);
+		CreateUserPanel.add(lastNameLabel);
 		CreateUserPanel.add(lastName);
+		CreateUserPanel.add(nickNameLabel);
+		CreateUserPanel.add(nickName);
+		CreateUserPanel.add(emailLabel);
+		CreateUserPanel.add(email);
 		CreateUserPanel.add(userRegisterBtn);
 		
+
+
 		// Methode die aufgerufen wird bei Clickhandler 
 		userRegisterBtn.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				Window.alert("User wird angelegt.");
+				Window.alert("Ich tu so als würde ich nen User anlegen.");
 				
 			}
 		});
-		
-
-		lastName.setText("Nickname eingeben");
-		RootPanel.get("content_wrap").clear();
-		RootPanel.get("content_wrap").add(CreateUserPanel);
-	
+ 	
 	}	
 
 	
--- hdm-rms/src/de/hdm/rms/client/Hdm_rms.java
+++ hdm-rms/src/de/hdm/rms/client/Hdm_rms.java
@@ -1,7 +1,10 @@
 package de.hdm.rms.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import de.hdm.rms.client.CreateUser;
@@ -9,13 +12,13 @@ import de.hdm.rms.client.CreateUser;
 class Hdm_rms implements EntryPoint {
 	
 	
-	CreateUser Affe = new CreateUser();
-	
 	
 	public void onModuleLoad() {
 		
+		CreateUser CreateUserView = new CreateUser();
 		
-		RootPanel.get("content_wrap").add(Affe);
+		RootPanel.get("content_wrap").add(CreateUserView);
+
 		
 	}
 	
