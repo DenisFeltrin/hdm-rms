5d9253a32cacf8464b08b18b8ee1ee88837fcec0
Denis Feltrin <d.feltrin@icloud.com>
2015-01-06 17:33:27 +0100

    Report

--- src/de/hdm/rms/shared/ReportService.java
+++ src/de/hdm/rms/shared/ReportService.java
@@ -1,7 +1,15 @@
 package de.hdm.rms.shared;
 
-import com.google.gwt.user.client.rpc.RemoteService;
+import de.hdm.rms.shared.bo.User;
 
+import java.util.ArrayList;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("reportservice")
 public interface ReportService extends RemoteService {
+	ArrayList<User> getAllUsers() throws IllegalArgumentException;
+	User getOneUserIdByNickname(String selectedNickname);
 
 }
